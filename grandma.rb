5ce5439f95f4c3323baaeaee3abce95a6cd8c625
Nicholas  Luong <nckluong@gmail.com>
2018-08-12 23:54:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -22,7 +22,6 @@ def speak_to_grandma (phrase){
   elsif((phrase == phrase.upcase) && (phrase == "I LOVE YOU GRANDMA!")){
    puts "I LOVE YOU TOO PUMPKIN!"
   }
-  
   else{
     puts "NO, NOT SINCE 1938!"
 }
