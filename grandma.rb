df38d19e8e0cc750a338b902376fa3bb61d7d7a3
Nicholas  Luong <nckluong@gmail.com>
2018-08-12 23:58:49 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,14 +16,15 @@
 def speak_to_grandma(phrase)
   
   upper = phrase.upcase
-  if phrase != upper {
-    puts "HUH?! SPEAK UP, SONNY!"
+  if (phrase != upper) {
+    puts "HUH?! SPEAK UP, SONNY!";
   }
   elsif((phrase == upper) && (phrase == "I LOVE YOU GRANDMA!")){
-   puts "I LOVE YOU TOO PUMPKIN!"
+   puts "I LOVE YOU TOO PUMPKIN!";
   }
   else{
-    puts "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!";
+  }
 end
 
 
