0dad56a001b55f72d341f74f74061c85083b6145
Nicholas  Luong <nckluong@gmail.com>
2018-08-13 00:00:57 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -20,7 +20,7 @@ def speak_to_grandma(phrase)
   if (phrase != upper)
      "HUH?! SPEAK UP, SONNY!";
   
-  elsif((phrase == upper) && (phrase == "I LOVE YOU GRANDMA!"))
+  elsif(phrase == upper) && (phrase == "I LOVE YOU GRANDMA!")
     "I LOVE YOU TOO PUMPKIN!";
   
   else
